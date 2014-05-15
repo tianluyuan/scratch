cb847dfe9106f15d36c7cdd0f93f2a9654289fe4
Tianlu Yuan <tianlutheman@gmail.com>
2014-05-15 15:51:35 -0600

    added script to count number of ways you can break change

---
+++ break_change.py
@@ -0,0 +1,42 @@
+import copy
+
+change_units = (1,5,10,25,50)
+
+def sum_change(change_list):
+    sum = 0
+    for idx, n_coins in enumerate(change_list):
+        sum += change_units[idx] * n_coins
+    return sum
+
+cache = {}
+def break_change_wrapper(n_cents):
+    change = set([])
+    def break_change(n_cents,
+                     curr_change = [0]*len(change_units)):
+        if n_cents < 0:
+            return
+
+        if n_cents == 0:
+            change.add(tuple(curr_change))
+            return
+
+        curr_change_sum = sum_change(curr_change)
+        if not cache.has_key(curr_change_sum):
+            cache[curr_change_sum]= break_change_wrapper(curr_change_sum)
+
+        if cache.has_key(n_cents):
+            remainder_change = cache[n_cents]
+            for a_remainder in remainder_change:
+                change.add(tuple(sum(x) for x in zip(a_remainder, curr_change)))
+            return
+
+
+        for idx, coin in enumerate(change_units):
+            this_curr = copy.copy(curr_change)
+            this_curr[idx] += 1
+            break_change(n_cents - coin,
+                         curr_change = this_curr)
+
+    break_change(n_cents)
+    return change
+
