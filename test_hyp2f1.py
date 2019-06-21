58c87b074fd9b8a00fb007c165722c666d55b8c3
Tianlu Yuan <tianlutheman@gmail.com>
2019-06-21 12:46:42 -0500

    add recura

--- test_hyp2f1.py
+++ test_hyp2f1.py
@@ -1,7 +1,9 @@
 import timeit
 import numpy as np
 from mpmath import hyp2f1 as mph2f1
+from mpmath import mp
 from scipy.special import hyp2f1, gammaln, poch, gamma, eval_jacobi
+import tqdm
 
 
 def trans_15_3_3(a,b,c,z):
@@ -13,6 +15,7 @@ def trans_15_3_4(a,b,c,z):
 
 
 def trans_15_3_6(a,b,c,z):
+    # print a,b,a+b-c+1, 1-z, hyp2f1(a,b,a+b-c+1, 1-z), mph2f1(a,b,a+b-c+1, 1-z)
     return np.exp(pochln2(c-b, -a)+gammaln(c)-gammaln(c-a))*hyp2f1(a,b,a+b-c+1, 1-z)# +
                 # np.exp((c-a-b) * np.log(1-z)+gammaln(c)+gammaln(a+b-c)-gammaln(a)-gammaln(b))*hyp2f1(c-a,c-b,c-a-b+1,1-z))
 
@@ -61,37 +64,65 @@ def jacobi_15_4_6(a,b,c,z):
     return np.exp(gammaln(n+1)-pochln(alpha+1,n))*eval_jacobi(n,alpha,beta,1-2*z)
 
 
-b=46.5
+from functools32 import lru_cache
+
+@lru_cache(maxsize=None)
+def recura(a,b,c,z):
+    if not z < 0:
+        return hyp2f1(a,b,c,z)
+    if a>-39:
+        return hyp2f1(a,b,c,z)
+
+    a += 1
+    return ((a*(1-z))*recura(a+1,b,c,z)-(2*a-c+(b-a)*z) * recura(a,b,c,z))/(c-a)
+
+b=36.5
+ll=0
 z = -0.5
-st = 120
+st = 40
 en = 160
+step = 2
 
 start = timeit.default_timer()
-mpout = [mph2f1(-_, b, -_-1/2., z) for _ in range(st, en,2)]
+mpout = [mph2f1(-_, b, -_-ll-1/2., z) for _ in range(st, en,step)]
 end = timeit.default_timer()
 print 'mpmath took', end-start
 
 start = timeit.default_timer()
-spout = [hyp2f1(-_, b, -_-1/2., z) for _ in range(st, en,2)]
+spout = [hyp2f1(-_, b, -_-ll-1/2., z) for _ in range(st, en,step)]
 end = timeit.default_timer()
 print 'scipy took', end-start
 
 start = timeit.default_timer()
-tfout = [trans_15_3_6(-_, b, -_-1/2., z) for _ in range(st, en,2)]
+tfout = [trans_15_3_6(-_, b, -_-ll-1/2., z) for _ in range(st, en,step)]
 end = timeit.default_timer()
 print 'scipy transformed took', end-start
 
 start = timeit.default_timer()
-psout = [poch_sum_ln_15_4_1(-_, b, -_-1/2., z) for _ in range(st, en,2)]
+psout = [poch_sum_ln_15_4_1(-_, b, -_-ll-1/2., z) for _ in range(st, en,step)]
 end = timeit.default_timer()
 print 'scipy pochsum took', end-start
 
 start = timeit.default_timer()
-jbout = [jacobi_15_4_6(-_, b, -_-1/2., z) for _ in range(st, en,2)]
+rrout = [recura(-_, b, -_-ll-1/2., z) for _ in range(st, en,step)]
 end = timeit.default_timer()
-print 'scipy jacobi took', end-start
+print 'scipy recura took', end-start
 
 
-print 'mpmath    scipy    transformed    pochsum    jacobi'
-for m, s, t, p, j in zip(mpout, spout, tfout, psout, jbout):
+print 'mpmath    scipy    transformed    pochsum    recura'
+for m, s, t, p, j in zip(mpout, spout, tfout, psout, rrout):
     print m, s, t, p, j
+
+
+from itertools import product
+lls = range(0,200,4)
+kks = range(0,200,4)
+jjs = range(30, 41, 1)
+ms = np.linspace(-0.99,0.99,51)
+# mp.dps = 80
+for jj, kk, ll, m in tqdm.tqdm(product(jjs, kks, lls, ms), total=len(lls)*len(kks)*len(jjs)*len(ms)):
+    mh = mph2f1(-jj, kk+0.5, 0.5-jj-ll, -m)
+    relerr = (mh-hyp2f1(-jj, kk+0.5, 0.5-jj-ll, -m))/mh
+    if relerr > 0.05:
+        print -jj, kk+0.5, 0.5-jj-ll, -m, relerr
+        # break
