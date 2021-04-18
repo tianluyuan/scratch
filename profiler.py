3d685cc5517630ab495f3d348320656066ba7acb
Tianlu Yuan <5412915+tianluyuan@users.noreply.github.com>
2021-04-18 18:09:56 -0500

    more profiling fun

--- profiler.py
+++ profiler.py
@@ -1,11 +1,13 @@
 import numpy as np
+from numba import njit
 from scipy.integrate import dblquad
 from functools import cache
 from sphere.distribution import fb84
 
-k=68
-b=10
-n1,n2,n3=0.4,np.sqrt(1-0.4**2),0
+k=300
+b=100
+n1=1
+n2,n3=np.sqrt(1-n1**2),0
 m=-1
 FB8 = fb84(np.asarray([[1,0,0],[0,1,0],[0,0,1]]),
            k,b,m,np.asarray([n1,n2,n3]))
@@ -15,7 +17,6 @@ def normalize():
     return FB8.normalize(cache=dict())
 
 
-# @cache
 def _integrand(th, ph):
     cth = np.cos(th)
     sth = np.sin(th)
@@ -25,6 +26,20 @@ def _integrand(th, ph):
 
 
 def _integrand2(th, ph):
+    cth = np.cos(th)
+    sth = np.sin(th)
+    cph = np.cos(ph)
+    sph = np.sin(ph)
+    return sth*np.e**(k*(n1*cth+n2*sth*cph+n3*sth*sph)+b*sth**2*(cph**2-m*sph**2))
+
+
+@cache
+def _integrand3(th, ph):
+    return _integrand(th, ph)
+
+
+@njit
+def _integrand4(th, ph):
     return np.sin(th)*np.exp(k*(n1*np.cos(th)+n2*np.sin(th)*np.cos(ph)+n3*np.sin(th)*np.sin(ph))+b*np.sin(th)**2*(np.cos(ph)**2-m*np.sin(ph)**2))
 
 
